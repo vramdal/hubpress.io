7612c23c40c8a5cc17ee35387d9ce5dee8561e7b
Vidar Ramdal <vidar.ramdal@sparebank1.no>
2018-06-13 09:20:51 +0200

    Revert "Adding react-ws content"
    This reverts commit 4c17907511d898357f0b40f3c5562d2345b1ed72.

--- react-ws/tasks/task-6-babel/src/my-app.js
+++
@@ -1,7 +0,0 @@
-class MyClass extends React.Component {
-    render() {
-        return <div>Inne i min klasse</div>
-    }
-}
-
-ReactDOM.render(<MyClass/>, document.getElementById('my-app'));
--- react-ws/tasks/task-9-redux/src/movies/movie-reducer.js
+++
@@ -1,1 +0,0 @@
-// Oppgave 9a og 9b: implementer reducer her
